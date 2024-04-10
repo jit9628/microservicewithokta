f3d251856f1e09e0fe25f9f7ce1f04fb3de6a00a
DESKTOP-P5PQ5EQ\Precise <softwaree831@gmail.com>
2024-04-10 12:51:13 +0530

    first commit

---
+++ HotelService/HotelService/src/main/java/com/lcwd/hotel/entites/Hotel.java
@@ -0,0 +1,57 @@
+package com.lcwd.hotel.entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+	@Id
+	private String id;
+	private String name;
+	private String location;
+	private String about;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+}
